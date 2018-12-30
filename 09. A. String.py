4f0c9e9e7c2c8638dffc54d3b0e4c8d7a873e6da
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2018-12-30 23:53:51 +0600

    introduction to strings

---
+++ 09. A. String.py
@@ -0,0 +1,13 @@
+a= "Bangla"
+b = 'desh'
+print(a+b)
+print(type(a))
+
+a = "It's my home" # different type of quote
+b = 'It"s my love'
+c = 'It\'s my pride' #using backslash to escape
+print(a)
+print(b)
+print(c)
+a = "\\" #printing a backslash
+print("Print backslash: ", a)
---
+++ 09. B. String value access.py
@@ -0,0 +1,10 @@
+country = "Bangladesh"
+print(country[0])
+print(country[1])
+print(country[2])
+
+for i in country:
+    print(i, end="")
+print("")
+print("String size: ", len(country))
+print("%s is my the country I live in." %country)
