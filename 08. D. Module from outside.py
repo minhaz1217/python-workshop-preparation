505b89864d0189ba413760bb9939289f54b23a6c
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2019-01-02 00:17:25 +0600

    importing module from outside file

---
+++ 08. D. Module from outside.py
@@ -0,0 +1,12 @@
+from PersonClass import Person
+'''
+or we can import like
+from <fileLocation> import *
+to import everythin of that file
+useful to import all the functions on that class also
+'''
+human = Person("Jackie")
+human.sayName()
+
+
+justAMethod()
---
+++ PersonClass.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def sayName(self):
+        print("My name is", self.name)
+
+def justAMethod():
+    print("I'm just a method, don't mind me. ;)")
